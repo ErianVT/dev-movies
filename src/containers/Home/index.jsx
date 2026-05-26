6e9b7249c2c62b2563a856b6481f8d6ce37dfdf6
ErianVT <erianvieira.tome@yahoo.com>
2026-05-26 07:08:54 -0300

    Estilizando o poste do Filme

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 
 import api from '../../services/api'
-import { Background } from './styles'
+import { Background, Container, Info, Poster } from './styles'
 
 function Home() {
   const [movie, setMovie] = useState()
@@ -12,7 +12,7 @@ function Home() {
         data: { results }
       } = await api.get('/movie/popular')
 
-      setMovie(results[3])
+      setMovie(results[1])
     }
 
     getMovies()
@@ -24,8 +24,18 @@ function Home() {
         <Background
           img={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
         >
-          <h1>{movie?.title}</h1>
-          <p>{movie?.overview}</p>
+          <Container>
+            <Info>
+              <h1>{movie?.title}</h1>
+              <p>{movie?.overview}</p>
+            </Info>
+            <Poster>
+              <img
+                alt="capa do filme"
+                src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+              />
+            </Poster>
+          </Container>
         </Background>
       )}
     </>
